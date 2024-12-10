3f5b5245b9f927a079285e624b158ea98d621b00
Davide Barbagallo <barbang.davide@gmail.com>
2024-12-10 17:59:50 +0100

    docs: update code comment

--- final_exam.ino
+++ final_exam.ino
@@ -1,41 +1,41 @@
 /*
- * Title of the project: The Humidity Fan
- * 
- * Description:
- * At every loop, it use DHT11 sensor to detect temperature and humidity of the enviroment and display that on an LCD screen.
- * If humidity percentage is greater or equal to 65, a DC motor starts to move the attached fan.
- * 
- * Author: Davide Barbagallo
- * Creation date: 30 Nov 2024
- * Last Edit date: 09 Dec 2024
- * 
- * Use components:
- * - Arduino UNO
- * - Power Supply Module or External Battery
- * - Breadboard
- * - DC Motor with Fan
- * - DHT11 sensor
- * - LCD Screen
- * - Diode
- * - NPN transistor
- * - 330Ω resistence
- * - 220Ω resistence
- * - Potentiometer
- * - Wires
- * 
- * Connected pins:
- * - Digital Pin 2: DHT11 sensor
- * - Digital Pin 3: DC Motor
- * - Digital Pin 7: LCD Screen RS
- * - Digital Pin 8: LCD Screen Enable
- * - Digital Pin 9: LCD Screen D4
- * - Digital Pin 10: LCD Screen D5
- * - Digital Pin 11: LCD Screen D6
- * - Digital Pin 12: LCD Screen D7
- * Used libraries:
- * - LiquidCrystal by Arduino, Adafruit
- * - DHT11 by Dhruba Saha
- */
+  The Humidity Fan
+
+  At every loop, it use DHT11 sensor to detect temperature and humidity of the enviroment and display that on an LCD screen.
+  If humidity percentage is greater or equal to 65, a DC motor starts to move the attached fan.
+
+  This project was made with:
+  - Arduino UNO
+  - Power Supply Module or External Battery
+  - Breadboard
+  - DC Motor with Fan
+  - DHT11 sensor
+  - LCD Screen
+  - Diode
+  - NPN transistor
+  - 330Ω resistence
+  - 220Ω resistence
+  - Potentiometer
+  - Wires
+  and pins were connected to used component in this way
+  - Digital Pin 2: DHT11 sensor
+  - Digital Pin 3: DC Motor
+  - Digital Pin 7: LCD Screen RS
+  - Digital Pin 8: LCD Screen Enable
+  - Digital Pin 9: LCD Screen D4
+  - Digital Pin 10: LCD Screen D5
+  - Digital Pin 11: LCD Screen D6
+  - Digital Pin 12: LCD Screen D7
+
+  To make all components work, I used some external libraries:
+  - LiquidCrystal by Arduino, Adafruit
+  - DHT11 by Dhruba Saha
+  downloadable directly from Arduino IDE Library Manager
+
+  modified 10 Dec 2024
+  by Davide Barbagallo
+*/
+
 #include <DHT11.h> // Include header file of DHT11 library
 #include <LiquidCrystal.h> // Include header file of LiquidCrystal library
 
@@ -75,6 +75,7 @@ byte percentage_symbol[8] = {
   0b00000
 };
 
+// the setup function runs once when you press reset or power the board
 void setup() {
   lcd.begin(16, 2); // Define width / height of LCD screen
   lcd.clear(); // Clear every screen from LCD screen
@@ -85,6 +86,7 @@ void setup() {
   Serial.begin(9600); // Sets the data rate for serial data transmission
 }
 
+// the loop function runs over and over again forever
 void loop() {
   int temperature = 0; // Define a variable to store read temperature
   int humidity = 0; // Define a variable to store read humidity
@@ -110,7 +112,7 @@ void loop() {
       lcd.print("Humidity: ");
       lcd.print(humidity);
       lcd.write(byte(1)); // Print percentage symbol of LCD screen
-  } else { // If results != 0 than an error occurred and we cannot read new data
+  } else { // If results != 0 then an error occurred and we cannot read new data
       DHT11_ERROR_COUNT += 1; // Increment error count
       // Print read data on serial (useful for debugging)
       Serial.print("Temperature: ");
@@ -124,7 +126,7 @@ void loop() {
   // If read humidity is greater or equal to HUMIDITY_THRESHOLD (defined above) AND there aren't enough consecutive errors, then we can start the motor
   if (humidity >= HUMIDITY_THRESHOLD && DHT11_ERROR_COUNT < DHT11_ERROR_THRESHOLD) {
     analogWrite(MOTOR_PIN, 255); // Give a PWM pin an analogic value (255 in this case). It is not necessary to call pinMode before calling this function
-  } else { // If read humidity is less than HUMIDITY_THRESHOLD (defined above) OR there was enough consecutive errors, then we can stop the motor
+  } else { // If read humidity is less then HUMIDITY_THRESHOLD (defined above) OR there was enough consecutive errors, then we can stop the motor
     analogWrite(MOTOR_PIN, 0); // Give a PWM pin an analogic value (0 in this case). It is not necessary to call pinMode before calling this function
   }
 }
